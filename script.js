71acad233d50bf1403403dbb11fdabe4e48d79ca
Tantely Andrianarivola <tantely.and@onja.org>
2020-07-31 14:02:16 +0300

    creating and starting the weekend project

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-console.log('HELLO');
-
 const recipes = [
 	{
-		title: 'Eggs',
+		title: 'Chicken',
 		picture: 'https://bit.ly/2ZXyiKI',
 		author: 'Loïc',
 		difficulty: 'easy',
@@ -14,12 +12,12 @@ const recipes = [
 			'Wait, put them out',
 			'Add some salt on it',
 		],
-		id: 1596168482053,
+		id: "185234589",
 	},
 	{
 		title: 'Eggs',
 		picture: 'https://bit.ly/2ZXyiKI',
-		author: 'Loïc',
+		author: 'Boglo',
 		difficulty: 'easy',
 		timing: '15',
 		ingredients: ['eggs', 'salt', 'water'],
@@ -29,12 +27,12 @@ const recipes = [
 			'Wait, put them out',
 			'Add some salt on it',
 		],
-		id: 1596168522409,
+		id: "2425734",
 	},
 	{
-		title: 'My recipe',
+		title: 'Breadfruit',
 		picture: 'https://bit.ly/2ZXyiKI',
-		author: 'Loïc',
+		author: 'Smith',
 		difficulty: 'easy',
 		timing: '15',
 		ingredients: ['eggs', 'salt', 'water'],
@@ -44,15 +42,76 @@ const recipes = [
 			'Wait, put them out',
 			'Add some salt on it',
 		],
-		id: 1596168522409,
+		id: "334",
+	},
+	{
+		title: 'Romazava',
+		picture: 'https://bit.ly/2ZXyiKI',
+		author: 'Maconren',
+		difficulty: 'easy',
+		timing: '15',
+		ingredients: ['eggs', 'salt', 'water'],
+		steps: [
+			'Put a pan on the fire',
+			'Crack the eggs on it',
+			'Wait, put them out',
+			'Add some salt on it',
+		],
+		id: "443454",
 	},
 ];
 
-const renderCard = () => {
-	// check the recipes collection
-	// generate the HTML
-	// put it in the DOM
-};
 
 const generateButton = document.querySelector('button.generate');
+const container = document.querySelector('.container');
+const outerModal = document.querySelector('.outer-modal');
+const innerModal = document.querySelector('.inner-modal');
+
+const renderCard = () => {
+	// check the recipes collection
+	const arrayFromRecipe = Array.from(recipes);
+	const arrayLength = arrayFromRecipe.length;
+	recipes.forEach(recipe = recipe => {
+		const myHtml = `
+		<div class="dish-card" data-id="${recipe.id}">
+			<h3
+				class="dish-title
+				name="dish-tile">${recipe.title}</h3>
+			<img src = "${recipe.picture}">
+			<div class="timing_difficulty">
+				<p class="dish-timing">${recipe.timing}</p>
+				<p class="dish-difficulty">${recipe.difficulty}</p>
+			</div>
+			<button type="button" class="btn">More Info</button>
+		</div>
+		`
+		container.insertAdjacentHTML('beforeend', myHtml);
+	})
+		// generate the HTML
+			// put it in the DOM
+};
+
 generateButton.addEventListener('click', renderCard);
+ 
+const ShowDetailsBtn = document.querySelectorAll('.bnt');
+
+const recipeDetails = (e) => {
+	const recipeShowBtn = e.target.matches('.btn');
+	if (recipeShowBtn) {
+		const parent = e.target.closest(".dish-card");
+		const id = parent.dataset.id;
+		const recipe = recipes.find(singleRecipe => singleRecipe.id === id);
+		console.log(recipe);
+			const myHTML = `
+				<h2>${recipe.name}</h2>
+			`
+			console.log(recipe.name);
+			innerModal.insertAdjacentHTML("afterbegin", myHTML);
+		 }
+		 
+		 outerModal.classList.add("open")
+}
+
+window.addEventListener('click', recipeDetails);
+
+
