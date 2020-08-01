9287ee00f734c0d433e2dfcd8cfe8d182842eabe
Tantely Andrianarivola <tantely.and@onja.org>
2020-08-01 16:14:00 +0300

    added style to the html

--- script.js
+++ script.js
@@ -115,6 +115,7 @@ const escapeModal = (e) => {
 	}
 }
 
+//function to show the food modale
 const foodDetails = (e) => {
 	const recipeShowBtn = e.target.matches('.btn');
 	if (recipeShowBtn) {
@@ -181,48 +182,50 @@ window.addEventListener('click', foodDetails);
 const addArecipeBtn = document.querySelector('.add-recipe');
 const formSubmit = document.querySelector('.form');
 
-// const handleAddRecipeForm = (e) => {
-// 	const myForm =`
-// 	<form class="form">
-// 			<p>Your name :</p>
-// 			<input
-// 				class="input-form"
-// 				type="text"
-// 				id="name"
-// 				name="name"
-// 				placeholder="Enter your name here"
-// 				required
-// 			/>
-// 			<p>Please select your dish :</p>
-// 			<select name="dish" class="select-form" required>
-// 				<option value="romazava">Romazava</option>
-// 				<option value="koba">Koba</option>
-// 				<option value="ravitoto">Ravitoto</option>
-// 				<option value="mokary">Mokary</option>
-// 				<option value="achard">Achard</option>
-// 				<option value="masikita">Masikita</option>
-// 				<option value="sambos">Sambos</option>
-// 				<option value="mofo-baolina">Mofo baolina</option>
-// 				<option value="ranonapango">Ranonapango</option>
-// 			</select>
-// 			<p>Please select the size of your plate :</p>
-// 			<input type="radio" id="small" name="size" value="small" required />
-// 			<label for="small">Small</label><br />
-// 			<input type="radio" id="medium" name="size" value="medium" />
-// 			<label for="medium">Medium</label><br />
-// 			<input type="radio" id="large" name="size" value="large" />
-// 			<label for="large">Large</label><br />
-// 			<p>How many pieces do you want to order?</p>
-// 			<input
-// 				class="input-form"
-// 				type="number"
-// 				id="quantity"
-// 				name="amount"
-// 				placeholder="Enter a number here"
-// 				required
-// 			/>
-// 			<button class="submitOrder" type="submit">Add this order</button>
-// 		</form>
-// 	`
-// }
-// addArecipeBtn.addEventListener('click', handleAddRecipeForm)
+const handleAddRecipeForm = (e) => {
+	const myForm =`
+	<form class="form">
+			<p>Your name :</p>
+			<input
+				class="input-form"
+				type="text"
+				id="name"
+				name="name"
+				placeholder="Enter your name here"
+				required
+			/>
+			<p>Please select your dish :</p>
+			<select name="dish" class="select-form" required>
+				<option value="romazava">Romazava</option>
+				<option value="koba">Koba</option>
+				<option value="ravitoto">Ravitoto</option>
+				<option value="mokary">Mokary</option>
+				<option value="achard">Achard</option>
+				<option value="masikita">Masikita</option>
+				<option value="sambos">Sambos</option>
+				<option value="mofo-baolina">Mofo baolina</option>
+				<option value="ranonapango">Ranonapango</option>
+			</select>
+			<p>Please select the size of your plate :</p>
+			<input type="radio" id="small" name="size" value="small" required />
+			<label for="small">Small</label><br />
+			<input type="radio" id="medium" name="size" value="medium" />
+			<label for="medium">Medium</label><br />
+			<input type="radio" id="large" name="size" value="large" />
+			<label for="large">Large</label><br />
+			<p>How many pieces do you want to order?</p>
+			<input
+				class="input-form"
+				type="number"
+				id="quantity"
+				name="amount"
+				placeholder="Enter a number here"
+				required
+			/>
+			<button class="submitOrder" type="submit">Add this order</button>
+		</form>
+	`
+	openModal();
+	innerModal.innerHTML = myForm;
+}
+addArecipeBtn.addEventListener('click', handleAddRecipeForm)
